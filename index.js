adad8a3199f5e3d86985aa5afb446774f00e8952
Marcus Mors <alivezeh@gmail.com>
2020-11-11 20:31:22 -0500

    base64 decoding doesn't work correctly with url'stest.js

--- index.js
+++ index.js
@@ -111,22 +111,3 @@ function listLabels(auth) {
 		}
 	)
 }
-
-// Each API may support multiple version. With this sample, we're getting v3 of the blogger API, and using an API key to authenticate.
-const blogger = google.blogger({
-	version: "v3",
-	auth: "AIzaSyCGXCtc65WKVPwD4ShcuahvZiZzFN7M4mw",
-})
-
-const params = {
-	blogId: "9419856",
-}
-
-// get the blog details
-blogger.blogs.get(params, (err, res) => {
-	if (err) {
-		console.error(err)
-		throw err
-	}
-	console.log(`The blog url is ${res.data.url}`)
-})
---
+++ test.js
@@ -0,0 +1,38 @@
+// const Buffer = require("buffer")
+
+const text1 =
+	"DQpDQ09NUDItMSAtIDU4MzIyIC0-IFRhcmVhIC0-IFJlYWxpemFyIGludmVzdGlnYWNpw7NuIHNvYnJlIExlbmd1YWplcyBkZQ0KUHJvZ3JhbWFjacOzbg0KLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tDQpLZWxseSBWaXpjb25kZSBMYSBNb3R0YSBoYSBoZWNobyB1biBjb21lbnRhcmlvIGRlIHJldHJvYWxpbWVudGFjacOzbiBlbiBzdQ0KZW50cmVnYSBkZSBsYSB0YXJlYSBSZWFsaXphciBpbnZlc3RpZ2FjacOzbiBzb2JyZSBMZW5ndWFqZXMgZGUNClByb2dyYW1hY2nDs24uDQoNClB1ZWRlIHZlcmxhIGFkanVudGEgYSBzdSBlbnRyZWdhIGVuDQoNCmh0dHBzOi8vdmlydHVhbC51Y3NwLmVkdS5wZS9tb2QvYXNzaWduL3ZpZXcucGhwP2lkPTQ1ODM2Mw0mdXRtX3NvdXJjZT1zZW5kZ3JpZC5jb20mdXRtX21lZGl1bT1lbWFpbCZ1dG1fY2FtcGFpZ249d2Vic2l0ZQ0KDQotLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0NCg=="
+
+const text2 =
+	"DQpDQ09NUDItMSAtIDU4MzIyIC0-IFRhcmVhIC0-IFJlYWxpemFyIGludmVzdGlnYWNpw7NuIHNvYnJlIExlbmd1YWplcyBkZQ0KUHJvZ3JhbWFjacOzbg0KLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tDQpLZWxseSBWaXpjb25kZSBMYSBNb3R0YSBoYSBoZWNobyB1biBjb21lbnRhcmlvIGRlIHJldHJvYWxpbWVudGFjacOzbiBlbiBzdQ0KZW50cmVnYSBkZSBsYSB0YXJlYSBSZWFsaXphciBpbnZlc3RpZ2FjacOzbiBzb2JyZSBMZW5ndWFqZXMgZGUNClByb2dyYW1hY2nDs24uDQoNClB1ZWRlIHZlcmxhIGFkanVudGEgYSBzdSBlbnRyZWdhIGVuDQoNCmh0dHBzOi8vdmlydHVhbC51Y3NwLmVkdS5wZS9tb2QvYXNzaWduL3ZpZXcucGhwP2lkPTQ1ODM2Mw0mdXRtX3NvdXJjZT1zZW5kZ3JpZC5jb20mdXRtX21lZGl1bT1lbWFpbCZ1dG1fY2FtcGFpZ249d2Vic2l0ZQ0KDQotLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0NCg=="
+
+const text3 =
+	"DQoNCmh0dHBzOi8vdmlydHVhbC51Y3NwLmVkdS5wZS9tb2QvYXNzaWduL3ZpZXcucGhwP2lkPTQ1ODM2Mw0mdXRtX3NvdXJjZT1zZW5kZ3JpZC5jb20mdXRtX21lZGl1bT1lbWFpbCZ1dG1fY2FtcGFpZ249d2Vic2l0ZQ0KDQ"
+
+const text4 =
+	"DQoNCmh0dHBzOi8vdmlydHVhbC51Y3NwLmVkdS5wZS9tb2QvYXNzaWduL3ZpZXcucGhwP2lkPTQ1ODM2Mw"
+const text5 =
+	"PTQ1ODM2Mw0mdXRtX3NvdXJjZT1zZW5kZ3JpZC5jb20mdXRtX21lZGl1bT1lbWFpbCZ1dG1fY2FtcGFpZ249d2Vic2l0ZQ0KDQ"
+const text6 = "PTQ1ODM2Mw0mdXRtX3NvdXJjZ"
+
+//create buffer
+const bufferText1 = Buffer.from(text1, "base64")
+const bufferText2 = Buffer.from(text2, "base64")
+const bufferText3 = Buffer.from(text3, "base64")
+const bufferText4 = Buffer.from(text4, "base64")
+const bufferText5 = Buffer.from(text5, "base64")
+const bufferText6 = Buffer.from(text6, "base64")
+//decode buffer as UTF-8
+const decodedText1 = bufferText1.toString("ascii")
+const decodedText2 = bufferText2.toString("utf-8")
+const decodedText3 = bufferText3.toString("utf8")
+const decodedText4 = bufferText4.toString("utf8")
+const decodedText5 = bufferText5.toString("utf8")
+const decodedText6 = bufferText6.toString("utf8")
+
+console.log(decodedText1)
+console.log(decodedText2)
+console.log(decodedText3)
+console.log(decodedText4)
+console.log(decodedText5)
+console.log(decodedText6)
